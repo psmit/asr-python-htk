62a9ccd9cf8a28dc844212a1ebd6e9bd526fa103
Peter Smit <peter@cis.hut.fi>
2010-03-10 11:41:26 +0200

    Real training working now

---
+++ htk.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+import glob
+import shutil
+import os
+import job_runner
+
+num_tasks = 20
+extra_HTK_options = ["-A", "-D", "-V", "-T", "1"]
+
+default_config_file = None
+default_HERest_pruning = None
+
+
+
+def HERest(scpfile, source_hmm_dir, target_hmm_dir, phones_list, transcriptions, config = None, pruning = None):
+	global num_tasks, extra_HTK_options, default_config_file, default_HERest_pruning
+	
+	if config == None: config = default_config_file
+	if pruning == None: pruning = default_HERest_pruning
+	
+	# divide scp files over HERest tasks
+	split_file(scpfile, num_tasks)
+	
+	command = ["HERest"]
+	command.extend(extra_HTK_options)
+	
+	command.extend(["-C", config])
+	command.extend(["-I", transcriptions])
+	command.extend(["-t"])
+	command.extend(pruning)
+	command.extend(["-H", source_hmm_dir + "/macros"])
+	command.extend(["-H", source_hmm_dir + "/hmmdefs"])
+	command.extend(["-M", target_hmm_dir])
+	
+	# copy merge_command now because the last options are different
+	merge_command = list(command)
+	
+	command.extend(["-S", scpfile+ ".part.%t"])
+	command.extend(["-p", "%t"])
+	command.append(phones_list)
+	
+	job_runner.submit_job(command, {"numtasks": num_tasks})
+	
+	merge_command.extend(["-p", 0])
+	merge_command.append(phones_list)
+	merge_command.extend(glob.glob(target_hmm_dir+"/*.acc"))
+	
+	job_runner.submit_job(merge_command, {"numtasks": 1})
+
+	# remove acc files
+	#for file in glob.glob(target_hmm_dir+"/*.acc"): shutil.rm(file)
+	
+	# remove splitted scp files
+	#for file in glob.glob(scpfile+".part.*"): shutil.rm(file)
+	
+	
+def split_file(filename, parts):
+	targetfilenames = [filename + ".part." + str(i) for i in range(1,parts+1)]
+	targetfiles = [open(fname, 'w') for fname in targetfilenames]
+	
+	sourcefile = open(filename)
+	counter = 0
+	for line in sourcefile:
+		targetfiles[counter].write(line)
+		counter = (counter + 1)%parts
+	
+	for f in targetfiles: f.close()
--- htk_train.py
+++ htk_train.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python
 
 import job_runner
+import htk
 
-job_runner.default_options = {'numtasks': 20}
+scpfile = 'train.scp'
+source_hmm_dir = 'hmm02'
+target_hmm_dir = 'hmm03'
+phones_list = 'monophones0'
+transcriptions = 'mono.mlf'
+config = 'config'
+pruning = ["300.0", "500.0", "2000.0"]
 
-job_runner.submit_job(['pwd'], {'verbosity': 2, 'numtasks':22})
+job_runner.default_options["verbosity"] = 5
+
+htk.HERest(scpfile, source_hmm_dir, target_hmm_dir, phones_list, transcriptions, config, pruning)
+
+
+
+#job_runner.default_options = {'numtasks': 20}
+
+#job_runner.submit_job(['pwd'], {'verbosity': 2, 'numtasks':22})
 
 print "Finished!"
