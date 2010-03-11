4bdb87d99d962408918325476428c09025ab9d01
Peter Smit <peter@cis.hut.fi>
2010-03-11 09:14:39 +0200

    Added tritonarray.py script

---
+++ tritonarray.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+
+########################################################################
+# tritonarray.py
+#
+# Description:
+# ------------
+# This script will run an array job in Triton.
+# Let's say you want to run the command HCopy on 100 different input files, indexed from 1-100.
+# sbatch -N 4 -t 00:10:00 --mem-per-cpu=1000 tritonarray.py -o %c.e%j.%t -e %c.e%j.%t -T 1-100 -- HCopy -S inputfile.%t
+########################################################################
+from subprocess import *
+from optparse import OptionParser
+import os.path
+
+def main():
+	parser = getOptParser()
+	(options, args) = parser.parse_args()
+	
+	tarr = TritonArray(options, args)
+	tarr.run()
+	
+class TritonArray:
+	options = None
+	command = None
+	jobname = "NoJobName"
+	t_start = 1
+	t_end = 1
+	
+	def __init__(self, options, command):
+		self.options = options
+		self.command = command
+		# if a relative script is given, make it more absolute
+		if self.command[0][0] != '.' and self.command[0][0] != '/' and os.path.isfile(os.getcwd() + '/' + self.command[0]):
+			self.command[0] = os.getcwd() + '/' + self.command[0]
+		
+		# same with the error and output streams
+		if self.options.ostream[0] != '.' and self.options.ostream[0] != '/':
+			self.options.ostream = os.getcwd() + '/' + self.options.ostream
+		if self.options.estream[0] != '.' and self.options.estream[0] != '/':
+			self.options.estream = os.getcwd() + '/' + self.options.estream
+			
+		# If the outputstreams are just directories, add the default filename pattern
+		if os.path.isdir(self.options.ostream):
+			self.options.ostream = self.options.ostream + '/%c.o%j.%t'
+			
+		if os.path.isdir(self.options.estream):
+			self.options.estream = self.options.estream + '/%c.e%j.%t'
+			
+		
+		#create a nice job name
+		self.jobname = os.path.basename(command[0])
+		
+		self.parse_task_option()
+		
+	def parse_task_option(self):
+		s, e = self.options.tasks.split('-', 1)
+		self.t_start = int(s)
+		self.t_end = int(e)
+		
+		if self.t_start > self.t_end:
+			print "Start task is bigger than end task"
+			sys.exit(12)
+		
+	def run(self):
+		processes = {}
+		
+		for t in range(self.t_start, self.t_end+1)
+			srun_command = ['srun']
+			srun_command.extend(['-n1','-N1'])
+			srun_command.extend(['-o', self.replace_flags(self.options.ostream, t)])
+			srun_command.extend(['-e', self.replace_flags(self.options.estream, t)])
+			srun_command.extend(self.replace_flags(self.options.command, t))
+			processes[t] = Popen(srun_command)
+			time.sleep(1)
+		
+		for task, process in processes.items():
+			ret_code = process.wait()
+			if ret_code != 0:
+				print "Error: task " + str(task) + " failed with code " + str(ret_code)
+		
+		print "Finished!"
+				
+	def replace_flags(self, pattern, task):
+		ret = pattern
+		
+		if type(ret).__name__=='list':
+			ret = [str(item).replace('%t', str(task)) for item in ret]
+		else:
+			ret = ret.replace('%c', self.jobname)
+			ret = ret.replace('%t', str(task))
+			ret = ret.replace('%J', os.getenv("SLURM_JOBID"))
+		return ret
+			
+def getOptParser():
+	usage = "usage: %prog [options] -- command"
+	parser = OptionParser(usage=usage)
+	parser.add_option("-T", "--tasks", dest="tasks", help="Task definition. Standard format: n-m (e.g. 1-100)", default="1-1")
+	parser.add_option("-o", "--output-stream", dest="ostream", help="write outputstream to FILE (%c for command, %J for job id, %t for task id). If a directory is given, the default format is used in that directory", default="%c.o%j.%t", metavar="FILE")
+	parser.add_option("-e", "--error-stream", dest="estream", help="write outputstream to FILE (%c for command, %J for job id, %t for task id). If a directory is given, the default format is used in that directory", default="%c.e%j.%t", metavar="FILE")
+	return parser
+	
+if __name__ == "__main__":
+    main()
