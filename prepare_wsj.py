d475c4f990dcdccb5f38633b47092d6a85423933
Peter Smit <peter@cis.hut.fi>
2010-03-12 09:36:40 +0200

    Added job runner options

--- prepare_wsj.py
+++ prepare_wsj.py
@@ -3,12 +3,14 @@
 import job_runner
 import htk
 
-scpfile = 'train.scp'
-config = 'config'
+scpfile = 'combined_train.scp'
+config = 'config.hcopy'
 
 job_runner.default_options["verbosity"] = 5
+job_runner.default_options["memlimit"] = 1000
+job_runner.default_options["timelimit"] = "01:00:00"
 
-htk.num_tasks = 500
+htk.num_tasks = 100
 htk.HCopy(scpfile, config)
 
 print "Finished!"
