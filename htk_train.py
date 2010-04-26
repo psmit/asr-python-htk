62ff3f8a39171646366be8b3248cde26550464ac
Peter Smit <peter@cis.hut.fi>
2010-04-26 12:09:04 +0300

    Made sad re-estimate matrices every time

--- htk_train.py
+++ htk_train.py
@@ -299,7 +299,7 @@ for mix in [1, 2, 4, 6, 8, 12, 16]:
 ####################################
 #Speaker Adaptive Training
 ####################################
-for number_sat_round in range(0,2):
+for number_sat_round in range(0,4):
 
     current_step += 1
 
@@ -333,16 +333,16 @@ for number_sat_round in range(0,2):
         htk.HERest_estimate_transform(current_step, scpfile, source_hmm_dir, source_hmm_dir + '/cmllr', phones_list, transcriptions, ['config/config', cmllr_config], int(config.get('corpora', 'speaker_name_width')))
 
         logger.info("Start step: %d (%s)" % (current_step, 'Re-estimate model with HERest (SAT)'))
-        htk.HERest(current_step, scpfile, source_hmm_dir, target_hmm_dir, phones_list, transcriptions, False, ['config/config', cmllr_config], source_hmm_dir + '/cmllr',  int(config.get('corpora', 'speaker_name_width')))
-
-
-    current_step += 1
-
-    if current_step >= options.step:
-        logger.info("Start step: %d (%s)" % (current_step, 'Re-estimate model with HERest (SAT)'))
-        source_hmm_dir, target_hmm_dir = data_manipulation.createHmmDir(current_step)
-
         htk.HERest(current_step, scpfile, source_hmm_dir, target_hmm_dir, phones_list, transcriptions, True, ['config/config', cmllr_config], source_hmm_dir + '/cmllr',  int(config.get('corpora', 'speaker_name_width')))
+
+
+#    current_step += 1
+#
+#    if current_step >= options.step:
+#        logger.info("Start step: %d (%s)" % (current_step, 'Re-estimate model with HERest (SAT)'))
+#        source_hmm_dir, target_hmm_dir = data_manipulation.createHmmDir(current_step)
+#
+#        htk.HERest(current_step, scpfile, source_hmm_dir, target_hmm_dir, phones_list, transcriptions, True, ['config/config', cmllr_config], source_hmm_dir + '/cmllr',  int(config.get('corpora', 'speaker_name_width')))
 
 
 
