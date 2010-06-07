9e8fc5d61e18a4c40be9b18ae31e3acbab58469d
Peter Smit <peter@cis.hut.fi>
2010-06-07 12:46:10 +0300

    Add support for bl 2010 dataset of UEDIN

--- data_manipulation.py
+++ data_manipulation.py
@@ -671,7 +671,7 @@ def ued_bl_selection(ued_bl_dir, langs, persons):
     wav_files = []
     for lang in langs:
         for person in persons:
-            for file in glob.iglob(ued_bl_dir + "/downsampled_22kHz/*/*/%s*/%s/*0.wav"%(person,lang))
+            for file in glob.iglob(ued_bl_dir + "/downsampled_22kHz/*/*/%s*/%s/*0.wav"%(person,lang)):
                 if int(os.path.splitext(os.path.basename(file))[0][-6:-2]) < 126:
                     wav_files.append(file)
     return wav_files
