e460ea4eff6d3d0f1c8f338119442f4d8274cd12
Peter Smit <peter@cis.hut.fi>
2010-06-22 08:57:31 +0300

    add cancel handler

--- recognize_new.py
+++ recognize_new.py
@@ -318,16 +318,20 @@ def run_experiments(experiments,tasks_per_experiment=50,total_tasks=800,max_fail
         runnable_experiments = [experiment for experiment in experiments.values() if (not experiment.done) and experiment.fail_count < max_fail_count and experiment.are_dependencies_ok(experiments)]
 
 def signal_handler(signal, frame):
-    pass
+    global pool
+    if isinstance(pool, multiprocessing.Pool):
+        pool.terminate()
     #job_runner.signal_handler(signal, frame)
-    #sys.exit(254)
+    sys.exit(254)
 
-
+pool = None
 
 if __name__ == "__main__":
+    global pool
+
     #Register signal handlers
-    #signal.signal(signal.SIGINT, signal_handler)
-    #signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
 
     experiments = parse_config([])
     run_experiments(experiments)
