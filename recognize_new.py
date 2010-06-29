09b2f0828c58322784c2c547f19a83d2a0e7faec
Peter Smit <peter@cis.hut.fi>
2010-06-29 13:26:09 +0300

    New version of recognize_new.py

--- recognize_new.py
+++ recognize_new.py
@@ -340,7 +340,7 @@ def run_experiments(experiments,tasks_per_experiment=50,total_tasks=100,max_fail
         results = [pool.apply_async(experiment) for experiment in runnable_experiments]
 
         for result in results:
-            name, done = result.get()
+            name, done = result.get(99999999999)
             experiments[name].done = done
             if not done:
                 experiments[name].fail_count = experiments[name].fail_count + 1
