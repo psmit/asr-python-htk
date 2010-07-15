9265ecc162d715a9266277ec8e7178399d0c1d49
Peter Smit <peter@cis.hut.fi>
2010-07-15 15:07:47 +0300

    Make scp files absolute when copying

--- data_manipulation.py
+++ data_manipulation.py
@@ -621,7 +621,7 @@ def copy_scp_file(orig_file, new_file):
             p = ''
             if not line.startswith('/'):
                 p = os.path.dirname(orig_file) + '/'
-            print >> nf, p + line
+            print >> nf, p + line.rstrip()
 
 def write_mlf(transcriptions, mlf_file, extension = 'lab', include_sentence_boundaries = False, append=False):
     mode = 'w'
