e78b8da48713b08c34869b6a7f09c1977f678509
Peter Smit <peter@cis.hut.fi>
2010-11-04 15:52:39 +0200

    Fixed

--- recognize_new.py
+++ recognize_new.py
@@ -158,7 +158,7 @@ class Experiment(object):
             else:
                 with open(rescore_mlf, 'w') as out_file:
                     for line in open(hdecode_mlf):
-                        print >> out_file, line.replace(htk_lat_dir, '*')
+                        print >> out_file, line.rstrip().replace(htk_lat_dir, '*')
                 #shutil.copyfile(hdecode_mlf, rescore_mlf)
                 data_manipulation.mlf_to_trn(hdecode_mlf, recog_trn, speaker_name_width)
 
