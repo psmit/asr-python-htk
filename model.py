2ac609ab9af8bd16f6024089433740e76cdc002f
Peter Smit <peter@cis.hut.fi>
2011-02-09 12:27:18 +0200

    General refactoring

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ import shutil
 from gridscripts.remote_run import System
 from htk2.tools import HCompV,HERest,HHEd,HLEd,HVite, Copier
 from htk2.units import HTK_dictionary,HTK_transcription
-
+import htk_file_strings
 
 
 __author__ = 'peter'
@@ -47,7 +47,7 @@ class HTK_model(object):
         self.id = None
 
 
-    def get_model_name_id(self,prev=0,id=None):
+    def _get_model_name_id(self,prev=0,id=None):
         if id is None:
             id = self.id-prev
         return self.model_dir + '/' + "{0}.{1:02d}".format(self.name,id)
@@ -92,7 +92,7 @@ class HTK_model(object):
 
         self.id = 1
 
-        phones_list = self.get_model_name_id() + '.hmmlist'
+        phones_list = self._get_model_name_id() + '.hmmlist'
         with open(phones_list, 'w') as phones_desc:
             for p in self.phones:
                 print(p, file=phones_desc)
@@ -161,8 +161,7 @@ class HTK_model(object):
         mkmono = os.path.join(tmp_dir,'mkmono.led')
 
         with open(mkmono, 'w') as mkmono_desc:
-            print("""EX
-IS sil sil""" ,file=mkmono_desc)
+            print(htk_file_strings.MKMONO ,file=mkmono_desc)
         
             if use_sp:
                 self.training_phone_mlf = os.path.join(self.train_files_dir, 'phone1.mlf')
@@ -170,16 +169,13 @@ IS sil sil""" ,file=mkmono_desc)
                 self.training_phone_mlf = os.path.join(self.train_files_dir, 'phone0.mlf')
                 print("DE sp", file=mkmono_desc)
 
-        HLEd(self.htk_config, self.training_word_mlf, mkmono, self.get_model_name_id() + '.hmmlist', self.training_dict,self.training_phone_mlf).run()
-
+        HLEd(self.htk_config, self.training_word_mlf, mkmono, self._get_model_name_id() + '.hmmlist', self.training_dict,self.training_phone_mlf).run()
 
         shutil.rmtree(tmp_dir)
 
     def align_transcription(self):
         self.training_phone_mlf = os.path.join(self.train_files_dir, 'phone_aligned.mlf')
-
-#        htk.HVite(current_step, scpfile, target_hmm_dir, dict, phones_list, 'files/words.mlf', transcriptions)
-        HVite(self.htk_config, self.training_scp, self.get_model_name_id() + '.mmf', self.training_dict, self.get_model_name_id() + '.hmmlist', self.training_phone_mlf, self.training_word_mlf).run()
+        HVite(self.htk_config, self.training_scp, self._get_model_name_id() + '.mmf', self.training_dict, self._get_model_name_id() + '.hmmlist', self.training_phone_mlf, self.training_word_mlf).run()
 
     def flat_start(self):
         tmp_dir = System.get_global_temp_dir()
@@ -187,39 +183,12 @@ IS sil sil""" ,file=mkmono_desc)
         vFloors = os.path.join(tmp_dir, 'vFloors')
 
         with open(proto_file, 'w') as proto_desc:
-            print("""~o <VecSize> 39 <MFCC_0_D_A_Z>
-~h "proto"
-<BeginHMM>
-        <NumStates> 5
-        <State> 2
-                <Mean> 39
-0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0
-                <Variance> 39
-1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
-        <State> 3
-                <Mean> 39
-0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0
-                <Variance> 39
-1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
-        <State> 4
-                <Mean> 39
-0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0
-                <Variance> 39
-                1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0 1.0
-<TransP> 5
-0.0 1.0 0.0 0.0 0.0
-0.0 0.6 0.4 0.0 0.0
-0.0 0.0 0.6 0.4 0.0
-0.0 0.0 0.0 0.7 0.3
-0.0 0.0 0.0 0.0 0.0
-<EndHMM>""", file=proto_desc)
+            print(htk_file_strings.PROTO, file=proto_desc)
 
-#        runner = RemoteRunner()
-#        runner.run(HCompV(self.htk_config, self.training_scp, proto_file))
         HCompV(self.htk_config, self.training_scp, proto_file).run()
 
 
-        out_model = self.get_model_name_id() + ".mmf"
+        out_model = self._get_model_name_id() + ".mmf"
 
         model_def = ""
         phone_def = ""
@@ -237,7 +206,7 @@ IS sil sil""" ,file=mkmono_desc)
                 print(line.strip(), file=model_desc)
 
             #Write the hmmdefs  (replacing for each monophone, proto with the monophone)
-            for line in open( self.get_model_name_id() + '.hmmlist'):
+            for line in open( self._get_model_name_id() + '.hmmlist'):
                 print(phone_def.replace('proto', line.rstrip()), file=model_desc)
                 
         shutil.rmtree(tmp_dir)
@@ -245,20 +214,20 @@ IS sil sil""" ,file=mkmono_desc)
     def re_estimate(self,stats=False):
         self.id += 1
         if stats:
-            stats = self.get_model_name_id()+'.stats'
+            stats = self._get_model_name_id()+'.stats'
         else:
             stats = None
-        shutil.copyfile(self.get_model_name_id(1)+'.hmmlist',self.get_model_name_id()+'.hmmlist')
-        HERest(self.htk_config, self.training_scp,self.get_model_name_id(1)+'.mmf',self.get_model_name_id()+'.hmmlist',
-               self.training_phone_mlf,output_hmm_model=self.get_model_name_id()+'.mmf',stats=stats).run()
+        shutil.copyfile(self._get_model_name_id(1)+'.hmmlist',self._get_model_name_id()+'.hmmlist')
+        HERest(self.htk_config, self.training_scp,self._get_model_name_id(1)+'.mmf',self._get_model_name_id()+'.hmmlist',
+               self.training_phone_mlf,output_hmm_model=self._get_model_name_id()+'.mmf',stats=stats).run()
 
 
     def introduce_short_pause_model(self):
         self.id += 1
-        phones = [p.strip() for p in open(self.get_model_name_id(1)+'.hmmlist')]
+        phones = [p.strip() for p in open(self._get_model_name_id(1)+'.hmmlist')]
         phones.append('sp')
 
-        with open(self.get_model_name_id()+'.hmmlist', 'w') as phone_out:
+        with open(self._get_model_name_id()+'.hmmlist', 'w') as phone_out:
             for p in sorted(set(phones)):
                 print(p,file=phone_out)
 
@@ -268,8 +237,8 @@ IS sil sil""" ,file=mkmono_desc)
 
         state = ""
 
-        with open(self.get_model_name_id()+'.mmf', 'w') as model_desc:
-            for line in open(self.get_model_name_id(1)+'.mmf'):
+        with open(self._get_model_name_id()+'.mmf', 'w') as model_desc:
+            for line in open(self._get_model_name_id(1)+'.mmf'):
                 print(line, file=model_desc)
 
                 if line.startswith('~h'):
@@ -284,24 +253,16 @@ IS sil sil""" ,file=mkmono_desc)
             print(  "~h \"sp\" <BEGINHMM> <NUMSTATES> 3", file=model_desc)
             print( "<STATE> 2", file=model_desc)
             print(  state, file=model_desc)
-            print( """<TRANSP> 3
-             0.000000e+00 5.000000e-01 5.000000e-01
-             0.000000e+00 5.000000e-01 5.000000e-01
-             0.000000e+00 0.000000e+00 0.000000e+00
-            <ENDHMM>""", file=model_desc)
+            print( htk_file_strings.TRANSP3, file=model_desc)
 
         self.id += 1
-        shutil.copyfile(self.get_model_name_id(1)+'.hmmlist',self.get_model_name_id()+'.hmmlist')
+        shutil.copyfile(self._get_model_name_id(1)+'.hmmlist',self._get_model_name_id()+'.hmmlist')
 
 
         tmp_dir = System.get_global_temp_dir()
         with open(os.path.join(tmp_dir,'sil.hed'), 'w') as sil_desc:
-            print("""AT 2 4 0.2 {sil.transP}
-AT 4 2 0.2 {sil.transP}
-AT 1 3 0.3 {sp.transP}
-TI silst {sil.state[3],sp.state[2]}
-""", file = sil_desc)
-        HHEd(self.htk_config,self.get_model_name_id(1)+'.mmf', self.get_model_name_id()+'.mmf',self.get_model_name_id()+'.hmmlist',script=os.path.join(tmp_dir,'sil.hed')).run()
+            print( htk_file_strings.SIL_HED, file = sil_desc)
+        HHEd(self.htk_config,self._get_model_name_id(1)+'.mmf', self._get_model_name_id()+'.mmf',self._get_model_name_id()+'.hmmlist',script=os.path.join(tmp_dir,'sil.hed')).run()
         shutil.rmtree(tmp_dir)
 
         self.expand_word_transcription(True)
@@ -311,24 +272,68 @@ TI silst {sil.state[3],sp.state[2]}
         mktri = os.path.join(tmp_dir,'mktri.led')
 
         with open(mktri, 'w') as mktri_desc:
-            print("""ME sil sil sil
-WB sp
-NB sp
-TC sil sil""" ,file=mktri_desc)
+            print(htk_file_strings.MKTRI ,file=mktri_desc)
         old_mlf = self.training_phone_mlf
         self.training_phone_mlf = os.path.join(self.train_files_dir, 'tri.mlf')
         self.id += 1
-        HLEd(self.htk_config, old_mlf, mktri, self.get_model_name_id() + '.hmmlist', self.training_dict,self.training_phone_mlf).run()
-        self._remove_triphone_sil(self.get_model_name_id() + '.hmmlist', True)
+        HLEd(self.htk_config, old_mlf, mktri, self._get_model_name_id() + '.hmmlist', self.training_dict,self.training_phone_mlf).run()
+        self._remove_triphone_sil(self._get_model_name_id() + '.hmmlist', True)
         self._remove_triphone_sil(self.training_phone_mlf)
 
         tri_hed = os.path.join(tmp_dir,'tri.hed')
 
-        self._make_tri_hed(self.get_model_name_id() + '.hmmlist',self.get_model_name_id(1) + '.hmmlist',tri_hed)
+        self._make_tri_hed(self._get_model_name_id() + '.hmmlist',self._get_model_name_id(1) + '.hmmlist',tri_hed)
 
-        HHEd(self.htk_config,self.get_model_name_id(1)+'.mmf',self.get_model_name_id()+'.mmf',self.get_model_name_id(1)+'.hmmlist',script=tri_hed).run()
+        HHEd(self.htk_config,self._get_model_name_id(1)+'.mmf',self._get_model_name_id()+'.mmf',self._get_model_name_id(1)+'.hmmlist',script=tri_hed).run()
         
         shutil.rmtree(tmp_dir)    
+
+    def tie_triphones(self):
+        self.id += 1
+        tmp_dir = System.get_global_temp_dir()
+        full_list = os.path.join(tmp_dir,'full_list')
+
+        self._make_full_list(self._get_model_name_id(5) + '.hmmlist',full_list)
+
+        tree_hed = os.path.join(tmp_dir,'tree.hed')
+        self._make_tree_hed(self.htk_config.tying_rules, self._get_model_name_id(5) + '.hmmlist',tree_hed,
+                            self.htk_config.tying_threshold,self.htk_config.required_occupation,self._get_model_name_id(1) + '.stats',
+                            full_list,self._get_model_name_id() + '.hmmlist', os.path.join(tmp_dir,'trees'))
+
+        HHEd(self.htk_config,self._get_model_name_id(1) + '.mmf',self._get_model_name_id(0) + '.mmf',self._get_model_name_id(1) + '.hmmlist',script=tree_hed).run()
+
+        shutil.rmtree(tmp_dir)
+
+    def split_mixtures(self,num_mixes):
+        self.id += 1
+        tmp_dir = System.get_global_temp_dir()
+        hed_file =  os.path.join(tmp_dir,'mix.hed')
+
+        with open(hed_file, 'w') as hed:
+            print("MU {0:d} {{*.state[2-4].stream[1].mix}}".format(num_mixes),file=hed)
+            print("MU {0:d} {{sil.state[2-4].stream[1].mix}}".format(2) *num_mixes,file=hed)
+
+        shutil.copyfile(self._get_model_name_id(1) + '.hmmlist',self._get_model_name_id() + '.hmmlist')
+        HHEd(self.htk_config,self._get_model_name_id(1) + '.mmf',self._get_model_name_id(0) + '.mmf',self._get_model_name_id() + '.hmmlist',script=hed_file).run()
+        shutil.rmtree(tmp_dir)
+
+    def split_mixtures_variably(self,num_mixes, num_steps, step):
+        pass
+
+    def estimate_transform(self):
+        pass
+
+
+    def clean_up(self,keep_versions = None):
+        if keep_versions is None:
+            keep_versions = set()
+        else:
+            keep_versions = set(keep_versions)
+
+        for i in xrange(1,self.id):
+            if i not in keep_versions:
+                for file in glob.iglob(self._get_model_name_id(id=i)+'.*'):
+                    os.remove(file)
 
     @staticmethod
     def _remove_triphone_sil(file, unique = False):
@@ -344,44 +349,6 @@ TC sil sil""" ,file=mktri_desc)
                     print(line, file=wfile)
             if unique:
                 print("sil",file=wfile)
-
-    @staticmethod
-    def _make_tri_hed(triphones_list, phones_list, tri_hed):
-        with open(tri_hed, 'w') as trihed:
-            print("CL %s" % triphones_list,file=trihed)
-            for line in open(phones_list):
-                print("TI T_%(phone)s {(*-%(phone)s+*,%(phone)s+*,*-%(phone)s).transP}" % {'phone': line.rstrip()}, file=trihed)
-
-    def tie_triphones(self):
-        self.id += 1
-        tmp_dir = System.get_global_temp_dir()
-        full_list = os.path.join(tmp_dir,'full_list')
-
-        self._make_full_list(self.get_model_name_id(5) + '.hmmlist',full_list)
-
-        tree_hed = os.path.join(tmp_dir,'tree.hed')
-        self._make_tree_hed(self.htk_config.tying_rules, self.get_model_name_id(5) + '.hmmlist',tree_hed,
-                            self.htk_config.tying_threshold,self.htk_config.required_occupation,self.get_model_name_id(1) + '.stats',
-                            full_list,self.get_model_name_id() + '.hmmlist', os.path.join(tmp_dir,'trees'))
-
-        HHEd(self.htk_config,self.get_model_name_id(1) + '.mmf',self.get_model_name_id(0) + '.mmf',self.get_model_name_id(1) + '.hmmlist',script=tree_hed).run()
-
-        shutil.rmtree(tmp_dir)
-
-    @staticmethod
-    def _make_full_list(phone_list, full_list):
-        phones = []
-        for phone in open(phone_list):
-            if phone.rstrip() != 'sp': phones.append(phone.rstrip())
-
-        with open(full_list, 'w') as flist:
-            for phone1 in phones:
-                for phone2 in phones:
-                    if phone2 != 'sil':
-                        for phone3 in phones:
-                            print ("%s-%s+%s" % (phone1, phone2, phone3), file=flist)
-            print ('sp', file=flist)
-            print ('sil', file=flist)
 
     @staticmethod
     def _make_tree_hed(phone_rules_file, phones_list, tree_hed_file, tb, ro, statsfile, fulllist, tiedlist, trees):
@@ -400,58 +367,49 @@ TC sil sil""" ,file=mktri_desc)
         if phone_rules.has_key('sil'): del phone_rules['sil']
 
         with open(tree_hed_file, 'w') as tree_hed:
-            print("LS %s" % statsfile,file=tree_hed)
-            print("RO %.1f" % ro,file=tree_hed)
+            print("LS {0:>s}".format(statsfile),file=tree_hed)
+            print("RO {0:.1f}".format(ro),file=tree_hed)
             print("TR 0",file=tree_hed)
 
             for rule, phones in phone_rules.items():
-                print('QS "L_%s" {%s}' % (rule, ",".join([phone + '-*' for phone in phones])),file=tree_hed)
-                print('QS "R_%s" {%s}' % (rule, ",".join(['*+' + phone  for phone in phones])),file=tree_hed)
+                print('QS "L_{0:>s}" {{{1:>s}}}'.format(rule, ",".join([phone + '-*' for phone in phones])),file=tree_hed)
+                print('QS "R_{0:>s}" {{{1:>s}}}'.format(rule, ",".join(['*+' + phone  for phone in phones])),file=tree_hed)
 
 
             print("TR 2",file=tree_hed)
 
             for state in range(2,5):
                 for phone in open(phones_list):
-                    print('TB %(tb).1f "%(phone)s_s%(state)d" {("%(phone)s","*-%(phone)s+*","%(phone)s+*","*-%(phone)s").state[%(state)d]}' % {'tb': tb, 'state': state, 'phone': phone.rstrip()},file=tree_hed)
+                    print(
+                            'TB {tb:.1f} "{phone:>s}_s{state:d}" {{("{phone:>s}","*-{phone:>s}+*","{phone:>s}+*","*-{phone:>s}").state[{state:d}]}}'.format(
+                                    **{'tb': tb, 'state': state, 'phone': phone.rstrip()}),file=tree_hed)
 
             print("TR 1",file=tree_hed)
 
 
-            print('AU "%s"' % fulllist,file=tree_hed)
-            print('CO "%s"' % tiedlist,file=tree_hed)
-            print('ST "%s"' % trees,file=tree_hed)
+            print('AU "{0:>s}"'.format(fulllist),file=tree_hed)
+            print('CO "{0:>s}"'.format(tiedlist),file=tree_hed)
+            print('ST "{0:>s}"'.format(trees),file=tree_hed)
 
-    def split_mixtures(self,num_mixes):
-        self.id += 1
-        tmp_dir = System.get_global_temp_dir()
-        hed_file =  os.path.join(tmp_dir,'mix.hed')
-
-        with open(hed_file, 'w') as hed:
-            print("MU %d {*.state[2-4].stream[1].mix}" % num_mixes,file=hed)
-            print("MU %d {sil.state[2-4].stream[1].mix}" % 2*num_mixes,file=hed)
-
-        shutil.copyfile(self.get_model_name_id(1) + '.hmmlist',self.get_model_name_id() + '.hmmlist')
-
-        HHEd(self.htk_config,self.get_model_name_id(1) + '.mmf',self.get_model_name_id(0) + '.mmf',self.get_model_name_id() + '.hmmlist',script=hed_file).run()
-
-        shutil.rmtree(tmp_dir)
-
-    def split_mixtures_variably(self,num_mixes, num_steps, step):
-        pass
-
-    def estimate_transform(self):
-        pass
+    @staticmethod
+    def _make_tri_hed(triphones_list, phones_list, tri_hed):
+        with open(tri_hed, 'w') as trihed:
+            print("CL %s" % triphones_list,file=trihed)
+            for line in open(phones_list):
+                print("TI T_%(phone)s {(*-%(phone)s+*,%(phone)s+*,*-%(phone)s).transP}" % {'phone': line.rstrip()}, file=trihed)
 
 
-    def clean_up(self,keep_versions = None):
-        if keep_versions is None:
-            keep_versions = set()
-        else:
-            keep_versions = set(keep_versions)
+    @staticmethod
+    def _make_full_list(phone_list, full_list):
+        phones = []
+        for phone in open(phone_list):
+            if phone.rstrip() != 'sp': phones.append(phone.rstrip())
 
-        for i in xrange(1,self.id):
-            if i not in keep_versions:
-                for file in glob.iglob(self.get_model_name_id(id=i)+'.*'):
-                    os.remove(file)
-
+        with open(full_list, 'w') as flist:
+            for phone1 in phones:
+                for phone2 in phones:
+                    if phone2 != 'sil':
+                        for phone3 in phones:
+                            print ("{0:>s}-{1:>s}+{2:>s}".format(phone1, phone2, phone3), file=flist)
+            print ('sp', file=flist)
+            print ('sil', file=flist)
