df827346000c8aca4548cf3fa2e7268997fca39a
Peter Smit <peter@cis.hut.fi>
2011-02-25 12:23:46 +0200

    Update cleaner

--- cleaner.py
+++ cleaner.py
@@ -2,6 +2,8 @@
 import os
 import time
 
+print "Start cleaning"
+
 def rmtree(f,min_age):
     t = max(os.stat(f).st_mtime,os.stat(f).st_ctime)
 
@@ -20,17 +22,23 @@ def rmtree(f,min_age):
 
 global_delay = 23*60*60
 local_delay = 23*60*60
+log_delay = (3*24+23)*60*60
 
 if 'LOCAL_TMP' in os.environ and os.path.exists(os.environ['LOCAL_TMP']):
     for d in os.listdir(os.environ['LOCAL_TMP']):
         if d.startswith('tmp'):
             rmtree(os.path.join(os.environ['LOCAL_TMP'],d),local_delay)
             if not os.path.exists(os.path.join(os.environ['LOCAL_TMP'],d)):
-                print "%s deleted" % os.path.join(os.environ['LOCAL_TMP'],d)
+                print "{0:>s} deleted".format(os.path.join(os.environ['LOCAL_TMP'], d))
 
 elif 'GLOBAL_TMP' in os.environ:
     for d in os.listdir(os.environ['GLOBAL_TMP']):
         if d.startswith('tmp'):
             rmtree(os.path.join(os.environ['GLOBAL_TMP'],d),global_delay)
             if not os.path.exists(os.path.join(os.environ['GLOBAL_TMP'],d)):
-                print "%s deleted" % os.path.join(os.environ['GLOBAL_TMP'],d)
+                print "{0:>s} deleted".format(os.path.join(os.environ['GLOBAL_TMP'], d))
+        if d.startswith('log'):
+            rmtree(os.path.join(os.environ['GLOBAL_TMP'],d),log_delay)
+            if not os.path.exists(os.path.join(os.environ['GLOBAL_TMP'],d)):
+                print "{0:>s} deleted".format(os.path.join(os.environ['GLOBAL_TMP'], d))
+
