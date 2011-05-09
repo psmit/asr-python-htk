b16ba050574c84541466b4ffcf8bba350ca525b0
Peter Smit <peter@cis.hut.fi>
2011-05-09 11:02:02 +0300

    Add num adaptation files to recognize.py

--- recognize.py
+++ recognize.py
@@ -11,6 +11,7 @@ parser.add_option('--no-local', dest='local_allowed', default=True, action="stor
 parser.add_option('--eval-speaker-chars', dest='eval_speaker_chars', default=3, type='int')
 parser.add_option('--transform-speaker-chars', dest='transform_speaker_chars', default=3, type='int')
 parser.add_option('-t', '--accent-tree-size', dest='tree_size', default=256, type='int')
+parser.add_option('-a', '--num-adaptation-files', dest='num_adaptation_files', default=0,type='int')
 htk_config = htk_config(debug_flags=['-A','-V','-D','-T','1'])
 htk_config.add_options_to_optparse(parser)
 
@@ -27,9 +28,9 @@ recognizer = HTK_recognizer(htk_config,name,model,scp,dict,lm)
 recognizer.recognize(None,'baseline')
 
 recognizer.add_adaptation(scp,recognizer.name+'.baseline.mlf',num_speaker_chars=options.eval_speaker_chars)
-recognizer.add_adaptation(scp,recognizer.name+'.baseline.mlf',num_speaker_chars=options.eval_speaker_chars,num_nodes=64)
+recognizer.add_adaptation(scp,recognizer.name+'.baseline.mlf',num_speaker_chars=options.eval_speaker_chars,num_nodes=options.num_adaptation_files)
 
-recognizer.recognize(None,'adapted')
+recognizer.recognize(None,'adapted%d'%options.num_adaptation_files)
 
 if len(args) > 6:
 
@@ -46,7 +47,7 @@ if len(args) > 6:
     recognizer.recognize(None,'transform')
 
     recognizer.add_adaptation(scp,recognizer.name+'.transform.mlf',num_speaker_chars=options.eval_speaker_chars)
-    recognizer.add_adaptation(scp,recognizer.name+'.transform.mlf',num_speaker_chars=options.eval_speaker_chars,num_nodes=64)
+    recognizer.add_adaptation(scp,recognizer.name+'.transform.mlf',num_speaker_chars=options.eval_speaker_chars,num_nodes=options.num_adaptation_files)
 
-    recognizer.recognize(None,'transform_stack')
+    recognizer.recognize(None,'transform_stack%d'%options.num_adaptation_files)
 
